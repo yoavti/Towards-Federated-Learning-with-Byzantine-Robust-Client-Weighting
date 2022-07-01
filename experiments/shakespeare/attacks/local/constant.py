28b29d16c45925a98c1b9a43e9322e36e3c306f5
yoavti <yoavti@post.bgu.ac.il>
2022-07-01 13:33:05 +0300

    Added constant.py

---
+++ experiments/shakespeare/attacks/local/constant.py
@@ -0,0 +1,10 @@
+from .base import LocalAttack
+import tensorflow as tf
+
+
+class ConstantAttack(LocalAttack):
+  def __int__(self, scalar):
+    self._scalar = scalar
+
+  def attack(self, weights_delta):
+    return tf.nest.map_structure(lambda _: _ - _ + self._scalar, weights_delta)
