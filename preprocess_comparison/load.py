5ecfd7ae5a38488ea422ec7bde4aa2e2bbd566ac
yoavti <yoavti@post.bgu.ac.il>
2022-07-14 12:42:31 +0300

    Added get_client_weights

--- preprocess_comparison/load.py
+++ preprocess_comparison/load.py
@@ -1,6 +1,7 @@
 import os
 
 import tensorflow_federated as tff
+import numpy as np
 
 
 dataset_modules = {'cifar100': tff.simulation.datasets.cifar100,
@@ -14,3 +15,10 @@ def load_dataset(name):
     module = dataset_modules[name]
     cache_dir = os.path.join('preprocess_comparison', name, 'cache')
     return module.load_data(cache_dir=cache_dir)[0]
+
+
+def get_client_weights(dataset, limit_count=None):
+    client_datasets = dataset.datasets(limit_count=limit_count)
+    weights = [len(list(client_dataset)) for client_dataset in client_datasets]
+    weights = np.array(weights)
+    return weights
