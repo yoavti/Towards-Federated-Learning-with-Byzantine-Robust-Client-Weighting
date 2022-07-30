e6e34a79ed3029a11ad0f153e6820a1fab19ca31
yoavti <yoavti@post.bgu.ac.il>
2022-07-30 19:23:46 +0300

    Added attacks to package

---
+++ attacks/simple_heuristic_omniscient/__init__.py
@@ -0,0 +1,5 @@
+from attacks.simple_heuristic_omniscient.delta_to_zero import DeltaToZeroAttack
+from attacks.simple_heuristic_omniscient.paf import ScalarPAFAttack
+
+
+ATTACKS = {'delta_to_zero': DeltaToZeroAttack, 'paf': ScalarPAFAttack}
