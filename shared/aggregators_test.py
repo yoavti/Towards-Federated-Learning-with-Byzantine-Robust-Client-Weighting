3897df9d25967cb82674210d7366ad357f26c22a
yoavti <yoavti@post.bgu.ac.il>
2022-07-30 15:17:28 +0300

    Added aggregators_test.py

--- shared/aggregators_test.py
+++ shared/aggregators_test.py
@@ -13,22 +13,19 @@
 # limitations under the License.
 
 import unittest
-import numpy as np
-from experiments.training.attacks.local import ConstantAttack, SignFlipAttack
+from shared.aggregators import mean, median, trimmed_mean
 
 
-def gen_rand_vec(dn=10):
-  return np.random.randn(dn)
+class AggregatorsTest(unittest.TestCase):
+  def test_mean(self):
+    self.assertEqual(mean([80, 90], [20, 30]), 86)
 
+  def test_median(self):
+    self.assertEqual(median(list(range(1, 5)), [0.25] * 4), 2.5)
+    self.assertEqual(median([1, 2, 2.5, 3, 4], [0.25, 0.25, 0, 0.25, 0.25]), 2.5)
 
-class LocalAttacksTest(unittest.TestCase):
-  def test_constant(self):
-    const = 100
-    np.testing.assert_array_almost_equal(const, ConstantAttack(const)(gen_rand_vec()))
-
-  def test_sign_flip(self):
-    v = gen_rand_vec()
-    np.testing.assert_array_almost_equal(-v, SignFlipAttack()(v))
+  def test_trimmed_mean(self):
+    self.assertEqual(trimmed_mean(list(range(1, 11)), None, 0.1), sum([2, 2, 3, 4, 5, 6, 7, 8, 9, 9]) / 10)
 
 
 if __name__ == '__main__':
