3bf1cbdd81926181b2a503818c21d7ce7f6bef2c
yoavti <yoavti@post.bgu.ac.il>
2022-07-31 12:45:39 +0300

    Fixed import of ALL_ATTACKS

--- shared/attacks/spec.py
+++ shared/attacks/spec.py
@@ -1,14 +1,12 @@
 import attr
 from shared.flags_validators import add_exception, create_in_validator
-
-
-ATTACKS = ['local']
+from shared.attacks.dict import ALL_ATTACKS
 
 
 @attr.s(eq=False, order=False, frozen=True)
 class AttackSpec(object):
   """Contains information for configuring attacks."""
   attack: str = attr.ib(
-    default='local',
-    validator=[attr.validators.instance_of(str), add_exception(create_in_validator(ATTACKS))])
+    default='sign_flip',
+    validator=[attr.validators.instance_of(str), add_exception(create_in_validator(ALL_ATTACKS))])
   """A string specifying which attack takes place."""
