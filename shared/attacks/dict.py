990c4064be89aa5e16287e08424372f084ba3ccc
yoavti <yoavti@post.bgu.ac.il>
2022-07-31 12:45:05 +0300

    Added dict.py to attacks module

---
+++ shared/attacks/dict.py
@@ -0,0 +1,4 @@
+from shared.attacks.collusion.attack_funcs.dict import COLLUSION_ATTACKS
+from shared.attacks.local.attack_funcs.dict import LOCAL_ATTACKS
+
+ALL_ATTACKS = {**COLLUSION_ATTACKS, **LOCAL_ATTACKS}
