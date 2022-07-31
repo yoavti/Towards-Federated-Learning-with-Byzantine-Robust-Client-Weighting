319f96b4675a5b4ef0dfdcdc78c03cf62f668bb5
yoavti <yoavti@post.bgu.ac.il>
2022-07-31 14:58:34 +0300

    Renamed configure_client_weight_fn to configure_client_weight

--- shared/client_weighting/configure.py
+++ shared/client_weighting/configure.py
@@ -4,7 +4,7 @@ from shared.client_weighting.spec import ClientWeightingSpec
 from shared.google_tff_research.utils import task_utils
 
 
-def configure_client_weight_fn(client_weight_spec: ClientWeightingSpec):
+def configure_client_weight(client_weight_spec: ClientWeightingSpec):
   client_weighting = client_weight_spec.client_weighting
   task = client_weight_spec.task
   if client_weighting is ClientWeighting.NUM_EXAMPLES and task in task_utils.TASKS_NUM_TOKENS:
