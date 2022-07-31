ab5e918c45333d03d701c8a02037eeb72d8e3170
yoavti <yoavti@post.bgu.ac.il>
2022-07-31 19:36:19 +0300

    Added import of ALL_AGGREGATORS

--- shared/aggregators/__init__.py
+++ shared/aggregators/__init__.py
@@ -1,2 +1,3 @@
 from shared.aggregators.numpy_aggregators import mean, median, trimmed_mean
 from shared.aggregators.factory import NumpyAggregationFactory, PreprocessedAggregationFactory, RobustWeiszfeldFactory
+from shared.aggregators.options import ALL_AGGREGATORS
