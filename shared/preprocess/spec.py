de52a35075051b4483f7efeef5e2cb62d0f27553
yoavti <yoavti@post.bgu.ac.il>
2022-07-31 11:19:20 +0300

    Added alpha, alpha_star, and all_weights

--- shared/preprocess/spec.py
+++ shared/preprocess/spec.py
@@ -1,5 +1,7 @@
 import attr
-from shared.flags_validators import add_exception, create_in_validator
+import numpy as np
+from typing import Optional
+from shared.flags_validators import add_exception, create_in_validator, check_proportion
 
 
 WEIGHT_PREPROCS = ['weight_preproc']
@@ -15,6 +17,9 @@ class PreprocessSpec(object):
     byzantines_part_of: A string specifying what whether preprocessing should
       take all clients into account or
       only those in the current round.
+    alpha: A float specifying Byzantine proportion.
+    alpha_star: A float specifying Byzantine weight proportion.
+    all_weights: An optional ndarray containing the weights of all clients
   """
   weight_preproc: str = attr.ib(
     default='weight_preproc',
@@ -22,3 +27,12 @@ class PreprocessSpec(object):
   byzantines_part_of: str = attr.ib(
     default='byzantines_part_of',
     validator=[attr.validators.instance_of(str), add_exception(create_in_validator(BYZANTINES_PART_OF))])
+  alpha: float = attr.ib(
+    default=0.1,
+    validator=[attr.validators.instance_of(float), add_exception(check_proportion)])
+  alpha_star: float = attr.ib(
+    default=0.1,
+    validator=[attr.validators.instance_of(float), add_exception(check_proportion)])
+  all_weights: Optional[np.ndarray] = attr.ib(
+    default=None,
+    validator=attr.validators.optional(attr.validators.instance_of(np.ndarray)))
