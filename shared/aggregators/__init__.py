6a65b456a51ba95b2a3f69bc2d0b0b0929367e0a
yoavti <yoavti@post.bgu.ac.il>
2022-07-31 22:22:22 +0300

    Updated import of AGGREGATORS and NumpyAggregationFactory

--- shared/aggregators/__init__.py
+++ shared/aggregators/__init__.py
@@ -1,4 +1,4 @@
 from shared.aggregators.numpy_aggregators import mean, median, trimmed_mean
-from shared.aggregators.factory import NumpyAggregationFactory, PreprocessedAggregationFactory, RobustWeiszfeldFactory
-from shared.aggregators.options import ALL_AGGREGATORS
+from shared.aggregators.numpy_aggregation_factory import NumpyAggregationFactory
+from shared.aggregators.options import AGGREGATORS
 from shared.aggregators.configure import configure_aggregator
