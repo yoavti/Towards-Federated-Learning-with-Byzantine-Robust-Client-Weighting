ece9d70c6d290bc8c4dab621bb34d46985a2015d
yoavti <yoavti@post.bgu.ac.il>
2022-07-31 22:29:18 +0300

    Replaced test_shakespeare with test_all, trying a variety of combinations

--- tests/test_cases/test_run_experiment.py
+++ tests/test_cases/test_run_experiment.py
@@ -17,6 +17,11 @@ import unittest
 
 from absl import app, flags
 
+from shared.client_weighting import CLIENT_WEIGHTING
+from shared.preprocess import PREPROC_TRANSFORMS
+from shared.aggregators import AGGREGATORS
+from shared.attacks import ALL_ATTACKS
+
 from experiments.training import run_experiment
 
 FLAGS = flags.FLAGS
@@ -30,16 +35,15 @@ FLAGS.server_learning_rate = 1.0
 FLAGS.server_sgd_momentum = 0.0
 
 
-def test_run_experiment(task, weight_preproc='none', client_weighting='num_examples', aggregation='mean',
-                        attack='sign_flip', num_byzantine=0.1, byzantines_part_of='round'):
-  print(task, weight_preproc, aggregation, attack)
+def test_run_experiment(task, client_weighting='num_examples', weight_preproc='none', aggregation='mean',
+                        attack='sign_flip', num_byzantine=0.1):
+  print(task, client_weighting, weight_preproc, aggregation, attack, num_byzantine)
   FLAGS.task = task
-  FLAGS.weight_preproc = weight_preproc
   FLAGS.client_weighting = client_weighting
+  FLAGS.weight_preproc = weight_preproc
   FLAGS.aggregation = aggregation
   FLAGS.attack = attack
   FLAGS.num_byzantine = num_byzantine
-  FLAGS.byzantines_part_of = byzantines_part_of
   FLAGS.root_output_dir = tempfile.mkdtemp()
   try:
     app.run(run_experiment.main)
@@ -48,8 +52,16 @@ def test_run_experiment(task, weight_preproc='none', client_weighting='num_examp
 
 
 class RunExperimentTest(unittest.TestCase):
-  def test_shakespeare(self):
-    test_run_experiment('shakespeare_character')
+  def test_all(self):
+    tasks = ['cifar100_image', 'emnist_autoencoder', 'emnist_character', 'shakespeare_character']
+    num_byzantines = [0.1, 2]
+    for task in tasks:
+      for client_weighting in CLIENT_WEIGHTING:
+        for weight_preproc in list(PREPROC_TRANSFORMS) + ['none']:
+          for aggregation in AGGREGATORS:
+            for attack in ALL_ATTACKS:
+              for num_byzantine in num_byzantines:
+                test_run_experiment(task, client_weighting, weight_preproc, aggregation, attack, num_byzantine)
 
 
 if __name__ == '__main__':
