8f069e5bd985993a7896c91f7edbc837b1f084ed
yoavti <yoavti@post.bgu.ac.il>
2022-07-31 14:57:57 +0300

    Added client_weighting/configure.py

---
+++ shared/client_weighting/configure.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+from tensorflow_federated.python.learning import ClientWeighting
+from shared.client_weighting.spec import ClientWeightingSpec
+from shared.google_tff_research.utils import task_utils
+
+
+def configure_client_weight_fn(client_weight_spec: ClientWeightingSpec):
+  client_weighting = client_weight_spec.client_weighting
+  task = client_weight_spec.task
+  if client_weighting is ClientWeighting.NUM_EXAMPLES and task in task_utils.TASKS_NUM_TOKENS:
+    def client_weight_fn(local_outputs):
+      return tf.cast(tf.squeeze(local_outputs['num_tokens']), tf.float32)
+  else:
+    client_weight_fn = client_weighting
+  return client_weight_fn
